470ef3bb187be9fb5b61ed0c794439f82d959d3d
sidde <sidde@Siddesh_Naikare>
2026-01-18 11:30:06 +0530

    Additaion test

---
+++ src/main/java/Githubtestpackage/githubclass.java
@@ -0,0 +1,18 @@
+package Githubtestpackage;
+
+public class githubclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 7;
+        int b = 5;
+
+        int sum = a + b;
+
+        System.out.println("Addition of a and b is: " + sum);
+		
+		
+	}
+
+}
